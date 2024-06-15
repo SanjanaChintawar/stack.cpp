eb93a777c783ea035486c0f661906b8e197108d1
Sanjana Sachin Chintawar <sanjanachintawar007@gmail.com>
2024-06-15 19:00:05 +0530

    Minimum Cost To Make String Valid

---
+++ findMinCost.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int findMinCost(string str)
+{
+
+    // odd condition
+    if (str.length() % 2 == 1)
+    {
+        return -1;
+    }
+
+    stack<char> s;
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+
+        if (ch == '{')
+        {
+            s.push(ch);
+        }
+        else
+        {
+            // ch is closed brace
+            if (!s.empty() && s.top() == '{')
+            {
+                s.pop();
+            }
+            else
+            {
+                s.push(ch);
+            }
+        }
+    }
+
+    int a = 0, b = 0; // a = closed brace & b = open brace
+    while (!s.empty())
+    {
+        if (s.top() == '{')
+        {
+            b++;
+        }
+        else
+        {
+            a++;
+        }
+        s.pop();
+    }
+    int ans = (a + 1) / 2 + (b + 1) / 2;
+    return ans;
+}
+
+int main()
+{
+    string str = "{{{{}}}}";
+    int min;
+
+    min = findMinCost(str);
+
+    cout << min;
+}
